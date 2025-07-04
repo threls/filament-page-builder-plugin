0d295002f41eb6371a1cb0d32f8f3e50f461a307
sabina <sabinahaloci@gmail.com>
2025-07-04 10:24:22 +0200

    TB-55 add new components

---
+++ src/Data/VideoEmbedderData.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Threls\FilamentPageBuilder\Data;
+
+
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+
+#[MapName(SnakeCaseMapper::class)]
+class VideoEmbedderData extends Data
+{
+    public function __construct(
+        public ?string $title,
+        public ?string $video,
+        public ?string $externalUrl,
+    )
+    {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            title: $data['title'] ?? null,
+            video: $data['image'] ?? null,
+            externalUrl: $data['external_url'] ?? null,
+        );
+    }
+}
