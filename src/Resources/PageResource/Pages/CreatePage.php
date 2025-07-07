a74b64a0005c30fb62062cc0219aa6586a6c76ae
ardit bleta <bletardit@gmail.com>
2025-07-07 12:01:03 +0200

    fix translation on pagebuilder

--- src/Resources/PageResource/Pages/CreatePage.php
+++ src/Resources/PageResource/Pages/CreatePage.php
@@ -3,9 +3,11 @@
 namespace Threls\FilamentPageBuilder\Resources\PageResource\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Pages\CreateRecord\Concerns\Translatable;
 use Threls\FilamentPageBuilder\Resources\PageResource;
 
 class CreatePage extends CreateRecord
 {
+    use Translatable;
     protected static string $resource = PageResource::class;
 }
--- src/Resources/PageResource/Pages/EditPage.php
+++ src/Resources/PageResource/Pages/EditPage.php
@@ -4,10 +4,12 @@ namespace Threls\FilamentPageBuilder\Resources\PageResource\Pages;
 
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Pages\EditRecord\Concerns\Translatable;
 use Threls\FilamentPageBuilder\Resources\PageResource;
 
 class EditPage extends EditRecord
 {
+    use Translatable;
     protected static string $resource = PageResource::class;
 
     protected function getHeaderActions(): array
