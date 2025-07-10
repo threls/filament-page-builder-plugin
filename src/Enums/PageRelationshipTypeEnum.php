819449a7ee8562d8fb7934c7b3dfe37440931454
sabina <sabinahaloci@gmail.com>
2025-07-10 23:56:41 +0200

    remove unnscessary relationships

--- src/Enums/PageRelationshipTypeEnum.php
+++ src/Enums/PageRelationshipTypeEnum.php
@@ -4,10 +4,6 @@ namespace Threls\FilamentPageBuilder\Enums;
 
 enum PageRelationshipTypeEnum: string
 {
-    case TESTIMONIAL = 'testimonial';
-    case FAQ = 'faq';
-    case EVENT = 'event';
-    case FACTS = 'facts';
     case CONTRIBUTIONS = 'contributions';
     case SOCIAL_LINKS = 'social_links';
     case BLOGS = 'blogs';
